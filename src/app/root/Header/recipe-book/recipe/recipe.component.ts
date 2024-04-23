2c404cfac35406679de6518719d30db6e5e44f47
WojciechFabi <130673263+WojciechFabi@users.noreply.github.com>
2024-04-23 23:23:21 +0200

    Update recipe.component.ts

--- src/app/root/Header/recipe-book/recipe/recipe.component.ts
+++ src/app/root/Header/recipe-book/recipe/recipe.component.ts
@@ -4,6 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css',
+   animations: [
+    trigger('recipe-list', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('void => *', [
+        style({
+          opacity: 0,
+        }),
+        animate(500),
+      ]),
+      transition('* => void', [
+        animate(
+          500,
+          style({
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class RecipeComponent implements OnInit {
   constructor() {}
